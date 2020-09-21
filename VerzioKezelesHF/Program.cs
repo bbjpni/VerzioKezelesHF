a820a6092fcd78b975ae8d0818e3ad2237a3dc26
Zsombor <brozsek.zsombor@petrik.hu>
2020-09-21 14:18:08 +0200

    Add project files.

---
+++ VerzioKezelesHF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
