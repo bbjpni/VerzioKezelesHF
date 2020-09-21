e8ed5ee16b73ca5a14e5ef01718a5dc82cf72748
Zsombor <brozsek.zsombor@petrik.hu>
2020-09-21 14:19:10 +0200

    10 szam atlaga

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -4,9 +4,27 @@ namespace VerzioKezelesHF
 {
     class Program
     {
+        static double beolvas(int szam)
+        {
+            int atlag = 0;
+            for (int i = 0; i < szam; i++)
+            {
+                int outside;
+                bool exit = false;
+                do
+                {
+                    Console.Write("\nAdjon meg egy egesz szamot: ");
+                    exit = Int32.TryParse(Console.ReadLine(), out outside);
+                } while (!exit);
+                atlag += outside;
+            }
+            Console.WriteLine("\nA(z) " + szam + " db szam atlaga: " + (double)atlag / szam);
+            return atlag / szam;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            beolvas(10);
         }
     }
 }
